bd1c06ebfe0141e8c757962c667e1ce1f74858b5
Vishwanath K <vishwanath.k@kloud9.nyc>
2018-04-05 20:26:30 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -35,7 +35,7 @@ func getPage(url string) string {
 var _ = Describe("Response", func() {
 	Context("check http response", func() {
 		It("get response", func() {
-			responseMsg := "Hello World-pass-canary"
+			responseMsg := "Hello World"
 			host := os.Getenv("HOST")
 			if host != "" {
 				Expect(getPage("http://" + host + ":8081")).To(Equal(responseMsg))
